613429d4b7153bccdec6e08286f02e7bbba60fe4
Can Ibanoglu <can.ibanoglu@gmail.com>
2019-11-17 03:47:46 +0100

    Initial API

--- index.js
+++ index.js
@@ -1,39 +1,100 @@
 #!/usr/bin/env node
-const {exec} = require('child_process');
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = function createTorrent(opts = {
-  announceUrls: [''],
-  comment: '',
-  skipCreationDate: false,
-  pieceLength: 18,
-  name: '',
-  fileName: '',
-  private: true,
-  sourceString: '',
-  verbase: false,
-  webSeedUrls: [''],
-}) {
+function expandHome(p) {
+  return p.startsWith("~/") ? path.join(process.env.HOME, p.slice(2)) : p;
+}
+
+module.exports = function createTorrent(
+  opts = {
+    announceUrls: [],
+    comment: "",
+    noDate: false,
+    pieceLength: 18,
+    name: "",
+    sourcePath: "",
+    output: "",
+    private: false,
+    webSeedUrls: []
+  }
+) {
   return new Promise((resolve, reject) => {
     const args = [];
-    args.push('-a');
-    args.push(opts.announceUrls.join(','))
-    args.push('-n');
-    args.push('test.torrent');
-    args.push('-p');
-    args.push('./');
 
-    exec(`./build/Release/mkt ${args.join(' ')}`, (error, stdout, stderr) => {
+    if (opts.announceUrls.length < 1) {
+      reject("You must supply at least one announce URL!");
+      return;
+    }
+    args.push("-a", opts.announceUrls.join(","));
+
+    if (opts.sourcePath.length < 1) {
+      reject("You must supply the source path!");
+      return;
+    }
+
+    const expandedPath = expandHome(opts.sourcePath);
+
+    let stat;
+    try {
+      console.log("stat!");
+      stat = fs.statSync(expandedPath);
+    } catch {
+      reject("Given source path doesn't exist!");
+      return;
+    }
+
+    if (opts.comment) {
+      args.push("-c", `"${opts.comment}"`);
+    }
+
+    if (opts.noDate) {
+      args.push("-d");
+    }
+
+    if (opts.pieceLength) {
+      args.push("-l", opts.pieceLength.toString());
+    }
+
+    if (opts.name) {
+      args.push("-n", opts.name);
+    }
+
+    if (opts.output) {
+      args.push("-o", opts.output);
+    }
+
+    if (opts.private) {
+      args.push("-p");
+    }
+
+    if (opts.webSeedUrls.length) {
+      let webSeeds = "";
+      if (stat.isFile()) {
+        webSeeds = opts.webSeedUrls
+          .map(x => path.join(x, path.basename(sourcePath)))
+          .join(",");
+      } else {
+        webSeeds = opts.webSeedUrls.join(",");
+      }
+      args.push("-w", webSeeds);
+    }
+
+    args.push(expandedPath);
+
+    const binaryPath = path.join(module.path, "build/Release/mkt");
+    exec(`${binaryPath} ${args.join(" ")}`, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         reject(`exec error: ${error}`);
         return;
       }
       console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
+      console.error(`stderr: ${stderr}`);
       resolve();
     });
-
   });
-}
+};
 
-console.log('hello there!');
+console.log("hello there!");
--- test.js
+++
@@ -1,7 +0,0 @@
-const mkt = require('./index.js');
-
-mkt({
-  announceUrls: ['http://localhost:32323/cantracker/announce'],
-}).then(() => {
-  console.log('here');
-});
